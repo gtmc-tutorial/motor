1e8ba158b2ca5fc7d410c6e856456fcb9724f49b
AUNT-PC\b4791 <b4791290en@gmail.com>
2017-11-28 13:35:22 +0800

    database debug add

--- app/src/main/java/com/cyut/motor/Structure/MaintainStructure.java
+++ app/src/main/java/com/cyut/motor/Structure/MaintainStructure.java
@@ -5,4 +5,19 @@ package com.cyut.motor.Structure;
  */
 
 public class MaintainStructure {
+    public String day;
+    public String Oil_gearoil_label;
+    public int Oil_gearoil_price;
+    public String Oil_gearoil_trip;
+
+    public MaintainStructure() {
+        // Default constructor required for calls to DataSnapshot.getValue(MapStructure.class)
+    }
+
+    public MaintainStructure(String day, String Oil_gearoil_label, int Oil_gearoil_price,String Oil_gearoil_trip) {
+        this.day = day;
+        this.Oil_gearoil_label = Oil_gearoil_label;
+        this.Oil_gearoil_price = Oil_gearoil_price;
+        this.Oil_gearoil_trip= Oil_gearoil_trip;
+    }
 }
