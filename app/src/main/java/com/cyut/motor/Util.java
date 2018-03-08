77056a674042bd313c3298b83833cbf0a9275941
s065 <walkmanw705@gmail.com>
2018-03-08 11:50:07 +0800

    test push 123

--- app/src/main/java/com/cyut/motor/Util.java
+++ app/src/main/java/com/cyut/motor/Util.java
@@ -9,6 +9,7 @@ import android.util.TypedValue;
 
 public class Util {
     //工具
+    //test123456789
     public static int getDP(Context context, int dp) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
     }
