f1928f2a33545e6a1b61c4c08aa0fa33b3db8b8d
s065 <walkmanw705@gmail.com>
2018-03-08 11:56:17 +0800

    test666

--- app/src/main/java/com/cyut/motor/Util.java
+++ app/src/main/java/com/cyut/motor/Util.java
@@ -9,7 +9,7 @@ import android.util.TypedValue;
 
 public class Util {
     //工具
-    //test123456789
+    //test123456789.6666666666
     public static int getDP(Context context, int dp) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
     }
