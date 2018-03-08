8636169cd87e824decbae5082a38c4f1c4759d90
s065 <walkmanw705@gmail.com>
2018-03-08 11:40:36 +0800

    push test

--- app/src/main/java/com/cyut/motor/Util.java
+++ app/src/main/java/com/cyut/motor/Util.java
@@ -8,7 +8,6 @@ import android.util.TypedValue;
  */
 
 public class Util {
-    //工具
     public static int getDP(Context context, int dp) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
     }
