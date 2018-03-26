c8532b8111b0fe003f715a1955b882db1a06d4d7
sebtembersnake <walkmanw705@gmail.com>
2018-03-26 23:49:56 +0800

    delete

--- app/src/main/java/com/cyut/motor/s065/BatteryActivity.java
+++ app/src/main/java/com/cyut/motor/s065/BatteryActivity.java
@@ -1,26 +1,20 @@
 package com.cyut.motor.s065;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
-import com.cyut.motor.Activity.MainActivity;
 import com.cyut.motor.R;
-import com.cyut.motor.Structure.GasStructure;
 import com.cyut.motor.Structure.PlaceStructure;
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
-import com.firebase.client.Query;
-import com.firebase.client.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -32,6 +26,7 @@ public class BatteryActivity extends AppCompatActivity {
     ArrayList<BTableAdapter.TableRow> table = new ArrayList<BTableAdapter.TableRow>();
 
     public static ArrayList<String> key_array = new ArrayList<>();
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
